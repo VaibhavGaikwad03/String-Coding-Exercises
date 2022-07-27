2ddfd8c9501d468b19f0f4d6cc9232eb5baefd4c
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2022-07-27 21:03:26 +0530

    Add files via upload

---
+++ Check_Alphabet.c
@@ -0,0 +1,50 @@
+/*
+Accepet character from user and check whether it is alphabet or not
+
+Input : F
+Output : It is an Alphabet
+Input : $
+Output : It is not an Alphabet
+
+*/
+
+#include<stdio.h>
+
+#define TRUE 1
+#define FALSE 0
+
+typedef int BOOL;
+
+BOOL CheckAlpha(char ch)
+{
+    if((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z'))
+    {
+        return TRUE;
+    }
+    else
+    {
+        return FALSE;
+    }
+}
+
+
+int main()
+{
+    char cValue = '\0';
+    BOOL bRet = FALSE;
+    printf("Enter the character : \n");
+    scanf("%c",&cValue);
+
+    bRet = CheckAlpha(cValue);
+
+    if(bRet == TRUE)
+    {
+        printf("It is an Alphabet\n");
+    }
+    else
+    {
+        printf("It is not an Alphabet\n");
+    }
+
+    return 0;
+}
