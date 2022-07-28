c5ba336b51168c6137f991a0647522107157e0a1
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2022-07-28 10:35:11 +0530

    Add files via upload

---
+++ Check_Special.c
@@ -0,0 +1,52 @@
+/*
+Accept character from user and check whether it is special symbol or not (!, @, #, $, %, ^, &, *)
+
+Input : %
+Output : It is special character 
+
+Input : d
+Output : It is not a special character
+
+*/
+
+#include<stdio.h>
+
+#define TRUE 1
+#define FALSE 0
+
+typedef int BOOL;
+
+BOOL CheckSpecial(char ch)
+{
+    if((ch == '!') || (ch == '@') || (ch == '#') || (ch == '$') || (ch == '%') || (ch == '^') || (ch == '&') || (ch == '*'))
+    {
+        return TRUE;
+    }
+    else
+    {
+        return FALSE;
+    }
+}
+
+
+int main()
+{
+    char cValue = '\0';
+    BOOL bRet = FALSE;
+
+    printf("Enter the character : \n");
+    scanf("%c",&cValue);
+
+    bRet = CheckSpecial(cValue);
+
+    if(bRet == TRUE)
+    {
+        printf("It is special character\n");
+    }
+    else
+    {
+        printf("It is not a special character\n");
+    }
+
+    return 0;
+}
