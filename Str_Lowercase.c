e2a8632325c5949a6f66f8269be9c9533f2af827
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2022-07-30 10:23:04 +0530

    Add files via upload

---
+++ Str_Lowercase.c
@@ -0,0 +1,35 @@
+/*
+Write a program which accepts string from the user and convert it into lowercase
+
+Input : "Marvellous Multi OS"
+Output : "marvellous multi os"
+
+*/
+
+#include<stdio.h>
+
+void StrLwrX(char *str)
+{
+    while(*str != '\0')
+    {
+        if((*str >= 'A') && (*str <= 'Z'))
+        {
+            *str = *str + 32;
+        }
+        str++;
+    }
+}
+
+int main()
+{
+    char cArr[20];
+
+    printf("Enter the string : \n");
+    scanf("%[^\n]s", cArr);
+
+    StrLwrX(cArr);
+
+    printf("Modified string is : %s\n",cArr);
+
+    return 0;
+}
---
+++ Str_Uppercase.c
@@ -0,0 +1,35 @@
+/*
+Write a program which accepts string from the user and convert it into uppercase
+
+Input : "Marvellous Multi OS"
+Output : "MARVELLOUS MULTI OS"
+
+*/
+
+#include<stdio.h>
+
+void StrUprX(char *str)
+{
+    while(*str != '\0')
+    {
+        if((*str >= 'a') && (*str <= 'z'))
+        {
+            *str = *str - 32;
+        }
+        str++;
+    }
+}
+
+int main()
+{
+    char cArr[20];
+
+    printf("Enter the string : \n");
+    scanf("%[^\n]s", cArr);
+
+    StrUprX(cArr);
+
+    printf("Modified string is : %s\n",cArr);
+
+    return 0;
+}
