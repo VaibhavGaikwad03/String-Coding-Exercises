a52c95d131bf0f3fbac3917e2ed2c684b92c529b
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2022-08-02 11:28:54 +0530

    Add files via upload

---
+++ Str_Cpy_Remove_Spaces.c
@@ -0,0 +1,39 @@
+/*
+Write a program which accepts string from the user and copy that characters of string into another string by removing all white spaces
+
+Input : "Marvel   lous Pyt  hon"
+
+Output : "MarvellousPython"
+
+*/
+
+#include<stdio.h>
+
+void StrCpyRmSpaces(char *src, char *dest)
+{
+    while(*src != '\0')
+    {
+        if(*src != ' ')
+        {
+            *dest = *src;
+            dest++;
+        }
+        src++;
+    }
+    *dest = '\0';
+}
+
+int main()
+{
+    char cArr[30];
+    char cBrr[30];
+
+    printf("Enter the string : \n");
+    scanf("%[^'\n']s",cArr);
+
+    StrCpyRmSpaces(cArr, cBrr);
+
+    printf("Copied and white sapaces removed string is : %s", cBrr);
+
+    return 0;
+}
