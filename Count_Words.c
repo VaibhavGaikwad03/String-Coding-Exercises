080599d644aeb418e87e8502cc71ca3c090a215c
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2023-02-02 16:38:18 +0530

    Add files via upload

---
+++ Count_Words.c
@@ -0,0 +1,38 @@
+/*
+
+Write a program which accepts string from the user and count the words in that string.
+
+Input : India is my country.
+Output : 4
+
+*/
+
+#include <stdio.h>
+
+int CountWords(char *str)
+{
+    int iCount = 1;
+
+    while (*str != '\0')
+    {
+        if (*str == ' ')
+            iCount++;
+        str++;
+    }
+    return iCount;
+}
+
+int main(void)
+{
+    char str[50];
+    int iRet = 0;
+
+    puts("Enter the string : ");
+    gets(str);
+
+    iRet = CountWords(str);
+
+    printf("Count of the words in input string is : %d\n", iRet);
+
+    return 0;
+}
